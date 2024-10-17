5870a9773465e64feb8b01972e612ed8b80a6308
crazytelli <crazytelli@proton.me>
2024-10-17 10:09:53 -0300

    added widgets for wifi ssid

--- slstatus/config.h
+++ slstatus/config.h
@@ -66,8 +66,9 @@ static const char unknown_str[] = "n/a";
 static const struct arg args[] = {
 	/* function format          argument */
 	//{ datetime, "%s",           "%F %T" },
-	//{ wifi_perc, "W: (%3s%% on ", "wlp8s0" },
-    //{ netspeed_rx, "%sB/s  ", "enp0s3" },
+	{ wifi_essid, "[%s]  ",  "wlp4s0" },
+	{ wifi_perc, "W:%3s%% ", "wlp4s0" },
+  { netspeed_rx, "[%sB/s]  ", "wlp4s0" },
 	{ cpu_perc, "[CPU:%s%%]   ", NULL	      },
 	{ ram_perc, "[RAM:%s%%]   ", NULL	      },
   { battery_perc, "[BAT:%s%%]   ", "BAT1" },
